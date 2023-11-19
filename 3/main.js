1ea3ae86330bb1cad44767fc2790a89b93a9b8bb
Kaveendra Randeera <khkrandeera@gmail.com>
2023-11-19 11:38:57 +0530

    Done 3

---
+++ 3/main.js
@@ -0,0 +1,19 @@
+const element = document.querySelector('div');
+element.innerText = 'New Content';
+element.innerHTML = '<ul>New Content -2 </ul>'
+
+e//lement.setAttribute('data-value'='123');
+
+const btn1 = document.querySelector('#btn1');
+btn1.removeAttribute('disabled');
+
+const inputElm = document.querySelector('input');
+
+const inputValue = inputElm.value;
+
+inputElm.value = 'new value';
+
+
+
+
+
